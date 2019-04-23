ae43c25b6966e2e84616ce0ef3319603dee370d6
LDorine <dodosav.fr@gmail.com>
2019-04-23 10:50:50 +0200

    add image name

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if(isset($_POST['submit'])) {
             if(in_array($file_ext,$allowed)) {
                 if($file_error === 0) {
                     if($file_size <= 1*1024*1024) {
-                        $file_name_new = uniqid('',true).'.'.$file_ext;
+                        $file_name_new = 'image'.uniqid('',true).'.'.$file_ext;
                         $file_destination = 'upload/'.$file_name_new;
     
                         if(move_uploaded_file($file_tmp,$file_destination)) {
